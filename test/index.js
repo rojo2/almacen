5d902bfbe175d1d4d3a3462ca5a8a98a450b5025
Alejandro Gómez <alejandro@dialelo.com>
2015-03-12 17:50:47 +0100

    Test that null values are also stored

--- test/index.js
+++ test/index.js
@@ -7,6 +7,7 @@ describe("Almacen", function() {
     almacen.local.set("boolean", true);
     almacen.local.set("array", [1,2,3,4,5,6]);
     almacen.local.set("object", { "foo": "bar" });
+    almacen.local.set("null", null);
 
     expect(almacen.local.has("date")).to.equal(false);
     expect(almacen.local.has("string")).to.equal(true);
@@ -14,12 +15,14 @@ describe("Almacen", function() {
     expect(almacen.local.has("boolean")).to.equal(true);
     expect(almacen.local.has("array")).to.equal(true);
     expect(almacen.local.has("object")).to.equal(true);
+    expect(almacen.local.has("null")).to.equal(true);
 
     expect(almacen.local.get("string")).to.equal("hello");
     expect(almacen.local.get("number")).to.equal(23);
     expect(almacen.local.get("boolean")).to.equal(true);
     expect(almacen.local.get("array")).to.have.property("length").to.equal(6);
     expect(almacen.local.get("object")).to.have.property("foo").to.equal("bar");
+    expect(almacen.local.get("null")).to.be.null;
 
   });
 
