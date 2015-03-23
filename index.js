8c503558fd219777be5a5da1d988630c19c25f37
ROJO 2 <hola@rojo2.com>
2015-03-23 11:24:23 +0100

    Added DataUtils, serialize and deserialize

--- index.js
+++ index.js
@@ -1,21 +1,53 @@
 var JSON_DATA_SCHEME = 'data:application/json,';
 
+var DataUtils = {
+  scheme: function(type, encoding) {
+    var str = 'data:' + type;
+    if (encoding !== undefined) {
+      str += ';' + encoding;
+    }
+    str += ',';
+    return str;
+  },
+  toData: function(data, type, encoding) {
+    return this.scheme(type, encoding) + data;
+  },
+  fromData: function(data, type, encoding) {
+    return data.substr(this.scheme(type, encoding).length);
+  },
+  is: function(data, type) {
+    var str = 'data:' + type;
+    return (data.substr(0, str.length) === str);
+  }
+};
+
 function Storage(storage) { this.storage = storage; }
 
 Storage.prototype = {
   constructor: Storage,
   storage: null, 
-  set: function(name,value) {
+  set: function(name, value, serializer) {
     if (typeof value !== 'string' && typeof value !== 'number' && typeof value !== 'boolean') {
-      value = JSON_DATA_SCHEME + JSON.stringify(value);
+      if (typeof serializer === 'function') {
+        value = serializer(value);
+      } else {
+        value = DataUtils.toData(
+          JSON.stringify(value), 
+          'application/json'
+        );
+      }
     }
     this.storage.setItem(name, value);
   },
-  get: function(name) {
+  get: function(name, deserializer) {
     var value = this.storage.getItem(name);
     if (value) {
-      if (value.substr(0,JSON_DATA_SCHEME.length) === JSON_DATA_SCHEME) {
-        return JSON.parse(value.substr(JSON_DATA_SCHEME.length));
+      if (typeof deserializer === 'function') {
+        return deserializer(value);
+      } else if (DataUtils.is(value, 'application/json')) {
+        return JSON.parse(
+          DataUtils.fromData(value, 'application/json')
+        );
       } else if (value === 'true' || value === 'false') {
         return value === 'true';
       } else if (/^-?[0-9]+(\.[0-9]+)?$/.test(value)) {
@@ -35,6 +67,11 @@ Storage.prototype = {
   }
 };
 
+var almacen = {
+  local: new Storage(window.localStorage),
+  session: new Storage(window.sessionStorage)
+};
+
 // UMD
 (function(root, factory) {
   if (typeof define === 'function' && define.amd) {
@@ -45,9 +82,5 @@ Storage.prototype = {
     root.almacen = factory();
   }
 }(this, function () {
-  return {
-    local: new Storage(window.localStorage),
-    session: new Storage(window.sessionStorage)
-  };
+  return almacen;
 }));
-
