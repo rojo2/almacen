a38402137faea0e47e348f94c57be68e984fd4bd
ROJO 2 <hola@rojo2.com>
2015-05-02 01:44:31 +0200

    Added method to save and restore an image from local and session storage

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-var JSON_DATA_SCHEME = 'data:application/json,';
+var JSON_DATA_SCHEME = "data:application/json,";
 
 var DataUtils = {
   scheme: function(type, encoding) {
-    var str = 'data:' + type;
+    var str = "data:" + type;
     if (encoding !== undefined) {
-      str += ';' + encoding;
+      str += ";" + encoding;
     }
-    str += ',';
+    str += ",";
     return str;
   },
   toData: function(data, type, encoding) {
@@ -16,40 +16,54 @@ var DataUtils = {
     return data.substr(this.scheme(type, encoding).length);
   },
   is: function(data, type) {
-    var str = 'data:' + type;
+    var str = "data:" + type;
     return (data.substr(0, str.length) === str);
   }
 };
 
-function Storage(storage) { this.storage = storage; }
+function Storage(storage) {
+  this.storage = storage; 
+}
 
 Storage.prototype = {
   constructor: Storage,
   storage: null, 
-  set: function(name, value, serializer) {
-    if (typeof value !== 'string' && typeof value !== 'number' && typeof value !== 'boolean') {
-      if (typeof serializer === 'function') {
-        value = serializer(value);
+  set: function(name, value) {
+    if (typeof value !== "string" && 
+        typeof value !== "number" && 
+        typeof value !== "boolean") {
+
+      if (value instanceof HTMLCanvasElement) {
+        value = value.toDataURL("image/png");
+      } else if (value instanceof HTMLImageElement) {
+        var canvas = document.createElement("canvas");
+        canvas.width = value.width;
+        canvas.height = value.height;
+        var context = canvas.getContext("2d");
+        context.drawImage(value, 0, 0);
+        value = canvas.toDataURL("image/png");
       } else {
         value = DataUtils.toData(
           JSON.stringify(value), 
-          'application/json'
+          "application/json"
         );
       }
     }
     this.storage.setItem(name, value);
   },
-  get: function(name, deserializer) {
+  get: function(name) {
     var value = this.storage.getItem(name);
     if (value) {
-      if (typeof deserializer === 'function') {
-        return deserializer(value);
-      } else if (DataUtils.is(value, 'application/json')) {
+      if (DataUtils.is(value, "application/json")) {
         return JSON.parse(
-          DataUtils.fromData(value, 'application/json')
+          DataUtils.fromData(value, "application/json")
         );
-      } else if (value === 'true' || value === 'false') {
-        return value === 'true';
+      } else if (DataUtils.is(value, "image/png")) {
+        var image = new Image();
+        image.src = value;
+        return image;
+      } else if (value === "true" || value === "false") {
+        return value === "true";
       } else if (/^-?[0-9]+(\.[0-9]+)?$/.test(value)) {
         return parseFloat(value);
       }
@@ -74,9 +88,9 @@ var almacen = {
 
 // UMD
 (function(root, factory) {
-  if (typeof define === 'function' && define.amd) {
+  if (typeof define === "function" && define.amd) {
     define([], factory);
-  } else if (typeof exports === 'object') {
+  } else if (typeof exports === "object") {
     module.exports = factory();
   } else {
     root.almacen = factory();
